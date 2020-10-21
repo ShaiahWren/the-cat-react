404442a5e1a668874471e21f1b8331741db3f70f
Shaiah Wren <sde2111@columbia.edu>
2020-10-21 15:38:18 -0400

    added input button

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -5,9 +5,19 @@ import { eat, play, nap, setName } from '../redux/actions';
 const Activity = ({activity, eat, play, nap, name, setName}) => {
     const [input, setInput] = useState('');
 
+    const _handleChange = (name) => {
+        setInput(name);
+    };
+
+    const _handleClick = () => {
+        setName(input)
+    }
+
     return (
     <>
         <h1>What is the cat doing now??</h1>
+        <input type="text" onChange={(e) => _handleChange(e.target.value)} value={input}/>
+        <button onClick={_handleClick}>Submit</button>
         <p>{name} is { activity} </p>
         <button type="button" onClick={() => eat()}>
             Eating
